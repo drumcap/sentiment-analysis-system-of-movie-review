0e3f13bbf640aa0c9aa817b167bc299b03792c94
David Yun <drumcap@gmail.com>
2018-04-20 17:42:06 +0900

    Refactoring

--- movie_scrap/settings.py
+++ movie_scrap/settings.py
@@ -21,7 +21,7 @@ NEWSPIDER_MODULE = 'movie_scrap.spiders'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
-LOG_FILE = 'scrapy.log'
+# LOG_FILE = 'scrapy.log'
 LOG_LEVEL = 'DEBUG'
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -34,8 +34,8 @@ LOG_LEVEL = 'DEBUG'
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
-DOWNLOAD_DELAY = 2
-CONCURRENT_REQUESTS_PER_DOMAIN = 6
+DOWNLOAD_DELAY = 0.2
+# CONCURRENT_REQUESTS_PER_DOMAIN = 6
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
@@ -77,16 +77,16 @@ ITEM_PIPELINES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 2
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
